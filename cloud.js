2ebd0e9683e8e6bfae0f16b764e04abc3e698890
zz <83402853@163.com>
2016-10-09 11:26:49 +0800

    change test

--- cloud.js
+++ cloud.js
@@ -4,7 +4,7 @@ var AV = require('leanengine');
  * 一个简单的云代码方法
  */
 AV.Cloud.define('hello', function(request, response) {
-  response.success('Hello world!');
+  response.success('Hello world my   !');
 });
 
 module.exports = AV.Cloud;
